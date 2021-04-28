425a20904f530917f0b48390b77353d6014887e9
Miro Paintola <miro.paintola@gmail.com>
2021-04-28 16:01:53 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [sites, setSites] = useState([]);
+  const [keyword, setKeyword] = useState();
+
+  // Default query on page load
+  useEffect(() => {
+      fetch("https://api.github.com/search/repositories?q=react")
+      .then(res => res.json())
+      .then(resData => setSites(resData.items))
+  }, [])
+
+  const inputChanged = (event) => {
+    setKeyword(event.target.value)
+  }
+
+  const findMatchingRepos = async () => {
+    const searchString = `https://api.github.com/search/repositories?q=${keyword}`
+    await fetch(searchString)
+    .then(res => res.json())
+    .then(data => setSites(data.items))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Repositories</h1>
+      <input value={keyword} onChange={inputChanged} />
+      <button onClick={findMatchingRepos}>Search</button>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>URL</th>
+        </tr>
+        <tbody>
+          {
+            sites.map((site, index) => 
+            <tr key={index}>
+              <td>{site.full_name}</td>
+              <td><a href={site.html_url}>{site.html_url}</a></td>
+            </tr>
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
